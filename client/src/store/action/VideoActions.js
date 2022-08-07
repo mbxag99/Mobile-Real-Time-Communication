40329b1877081c4a3e47b898aa1eba4f1644767a
mbxag99 <mbx.ag99@gmail.com>
2022-08-07 18:38:13 +0300

    redux setup

---
+++ client/src/store/action/VideoActions.js
@@ -0,0 +1,17 @@
+export const join_Room = (UserData, router) => async (dispatch) => {
+  try {
+  } catch (err) {}
+};
+export const add_Stream = (UserData, router) => async (dispatch) => {
+  try {
+  } catch (err) {}
+};
+export const add_my_Stream = (UserData, router) => async (dispatch) => {
+  try {
+  } catch (err) {}
+};
+
+export const add_remote_stream = (UserData, router) => async (dispatch) => {
+  try {
+  } catch (err) {}
+};
---
+++ client/src/store/reducer/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import VideoReducer from "./VideoReducer";
+export const reducers = combineReducers({
+  VideoReducer: VideoReducer,
+});
